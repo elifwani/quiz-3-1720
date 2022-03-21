66f49520ca4f776a122ee99dc2b2cabaa3c4719a
Elif <elifsabuncular8@gmail.com>
2022-03-21 19:49:00 -0400

    quiz3

--- extending_script.js
+++ extending_script.js
@@ -1,38 +1,40 @@
+// Does something when window loads (windows loads at the end of the document loading process)
 window.onload = () => {
-  render();
+  render(); // renders the scene
 };
 
-const models = [
+const models = [ // define a constant variable models
   {
-    url: './assets/myModel/scene.gltf',
-    scale: '0.5 0.5 0.5',
-    rotation: '0 225 0'
+    url: './assets/myModel/scene.gltf', // the model location
+    scale: '0.5 0.5 0.5', // scale of the model
+    rotation: '0 225 0' // rotation of the model
   },
 ];
 
-let modelIndex = 0;
-const setModel = (model, entity) => {
-  if (model.position) {
-    entity.setAttribute('position', model.position);
+let modelIndex = 0; // define modelIndex variable which is 0
+const setModel = (model, entity) => { // define a constant variable setModel with model and entity parameters
+  if (model.position) { // if model position is called
+    entity.setAttribute('position', model.position); // set entity's position attribute to the model's gps position
   }
 
-  entity.setAttribute('gltf-model', model.url);
+  entity.setAttribute('gltf-model', model.url); // else set entity's position attribute to the model's url
 };
 
-function render() {
-  const scene = document.querySelector('a-scene');
+// Declation of the render() function 
+function render() { 
+  const scene = document.querySelector('a-scene'); // defines a variable which contains a-scene info from the index.html file
 
-  navigator.geolocation.getCurrentPosition(function (position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+  navigator.geolocation.getCurrentPosition(function (position) { // the geolocation is your current position, getting the actual geolocation below
+    const latitude = position.coords.latitude; // define a constant variable with latitude of the location
+    const longitude = position.coords.longitude; // define a constant variable with longitude of the location
 
-    const model = document.createElement('a-entity');
-    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    const model = document.createElement('a-entity'); // define a constant variable model which contains a-entity info from the index.html file
+    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`); // set model's gps aattribute to the location we defined
 
-    setModel(models[modelIndex], model);
+    setModel(models[modelIndex], model); // set each model (if more than one)
 
-    model.setAttribute('animation-mixer', '');
+    model.setAttribute('animation-mixer', ''); // no animation
 
-    scene.appendChild(model);
+    scene.appendChild(model); // add the model to the scene
   });
 }
